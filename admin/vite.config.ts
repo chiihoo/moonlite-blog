18b5a6d0f26bfbd42878093fa77233cb2e1bb2ac
chiihooy <chiihooy@qq.com>
2021-02-18 10:37:02 +0800

    refactor: 更新依赖

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -5,7 +5,9 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-    '@': resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
   }
 })
